14dc0a62839ac3f0c7626e2e57781238833501e4
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-02 14:55:50 +0200

    Add files via upload

--- js/lesson9/count_balance/index.js
+++ js/lesson9/count_balance/index.js
@@ -1,18 +1,29 @@
 const getTotalRevenue = transactions => {
-let res = transactions.map( element => Object.entries(transactions[0]));
-console.log(res);
-console.log(res[0].flat());
-
- 
-
-  return;
+let amountArr = transactions.map( element => element.amount);
+// console.log(amountArr);
+let summaryBalance = amountArr.reduce((sum, amountBalance) =>{ 
+  return sum + amountBalance;
+});
+// console.log(summaryBalance);
+   return summaryBalance;
 };
 
 // examples
 const dayTransactions = [
-  { userId: 22, amount: 60, operation: 'sell' },
-  { userId: 22, amount: 160, operation: 'buy' },
-  { userId: 44, amount: 90, operation: 'sell' },
+  { 
+    userId: 22, 
+    amount: 60, 
+    operation: 'sell' 
+  },
+  { 
+    userId: 22, 
+    amount: 160, 
+    operation: 'buy' 
+  },
+  { 
+    userId: 44, 
+    amount: 90, 
+    operation: 'sell' 
+  },
 ];
-
 const result = getTotalRevenue(dayTransactions); // ==> 310
