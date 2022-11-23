d6c8d80234aef679ca0b9548bec10c6ca76a7c01
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-23 10:54:54 +0200

    Update index.js

--- js/lesson21-dom/task2-search_element/index.js
+++ js/lesson21-dom/task2-search_element/index.js
@@ -1,11 +1,11 @@
-const getTitleElement = () => {
+export const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 };
 getTitleElement();
 
-const getInputElement = () => {
+export const getInputElement = () => {
     const inputElem = document.querySelector('[type="text"]');
   console.dir(inputElem);
   return inputElem;
