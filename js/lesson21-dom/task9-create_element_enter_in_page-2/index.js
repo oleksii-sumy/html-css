36dd198d7bfca1eab07b229de6ef85c375ee4377
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-25 10:00:14 +0200

    Add files via upload

---
+++ js/lesson21-dom/task9-create_element_enter_in_page-2/index.js
@@ -0,0 +1,21 @@
+export const finishList = () => {
+  const firstElem = document.createElement('li');
+  firstElem.textContent = '1';
+  const placeForFirst = document.querySelector('.list');
+  placeForFirst.prepend(firstElem);
+
+  const fourthElem = document.createElement('li');
+  fourthElem.textContent = '4';
+  const placeForFourth = document.querySelector('.special');
+  placeForFourth.before(fourthElem);
+
+  const sixElem = document.createElement('li');
+  sixElem.textContent = '6';
+  const placeForSix = document.querySelector('.special');
+  placeForSix.after(sixElem);
+
+  const eightElem = document.createElement('li');
+  eight.textContent = '8';
+  const placeForEight = document.querySelector('.list');
+  placeForEight.append(eightElem);
+};
