81c655a95a0d0d1259ad2c5a109a53f819c6635f
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-25 13:12:38 +0200

    Update index.js

--- js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
+++ js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
@@ -1,5 +1,5 @@
 export const finishForm = () => {
   const element = document.querySelector('input');
   element.setAttribute('name', 'login');
-  element.setAttribute('type', 'password');
+  element.setAttribute('type', String);
 };
