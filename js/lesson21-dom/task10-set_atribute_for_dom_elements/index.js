dc93354143cf135bc50b934fdafe58dc563161bc
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-25 14:53:43 +0200

    Update index.js

--- js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
+++ js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
@@ -1,5 +1,10 @@
 export const finishForm = () => {
-  const element = document.querySelector('input');
-  element.setAttribute('name', 'login');
-  element.setAttribute('type', String);
+  const oldElemPlace = document.querySelector('[type="text"]');
+  oldElemPlace.setAttribute('type', 'passport');
+  oldElemPlace.setAttribute('name', 'password');
+  const placeForNewElem = document.querySelector('.login-form');
+  const newElem = document.createElement('input');
+  placeForNewElem.prepend(newElem);
+  newElem.setAttribute('type', 'text');
+  newElem.setAttribute('name', 'login');
 };
