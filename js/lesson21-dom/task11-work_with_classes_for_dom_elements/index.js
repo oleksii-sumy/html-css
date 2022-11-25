13b147ea8380cd5d738129de98060d7377dc87a0
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-25 19:53:33 +0200

    Add files via upload

---
+++ js/lesson21-dom/task11-work_with_classes_for_dom_elements/index.js
@@ -0,0 +1,17 @@
+export const manageClasses = () => {
+  const classOne = document.querySelector('.one');
+  classOne.classList.add('selected');
+
+  const classTwo = document.querySelector('.two');
+  classTwo.classList.remove('selected');
+
+  const classThree = document.querySelector('.three');
+  classThree.classList.toggle('three_done');
+
+  const classFour = document.querySelector('.four');
+  if (classFour.classList.contains('some_class')) {
+    classFour.classList.add('another_class');
+  }
+};
+
+manageClasses();
