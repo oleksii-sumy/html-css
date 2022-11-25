cec72f490b66f53868c5ed7ee302c1cda53bcb8e
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-11-25 11:31:08 +0200

    Add files via upload

--- js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
+++ js/lesson21-dom/task10-set_atribute_for_dom_elements/index.js
@@ -1,7 +1,9 @@
 export const finishForm = () => {
-  const element = document.querySelector('input');
-  element.setAttribute('name', 'login');
-  element.setAttribute('type', 'password');
+  const inputElem = document.querySelector('input');
+inputElem.outerHTML = `<input name="password" type="text" />`;
+const changeAtribute = document.querySelector('input');
+  changeAtribute.setAttribute('name', 'login');
+  changeAtribute.setAttribute('type', 'password');
 };
 
 finishForm();
