c37df524a75a454224eb2e4833ccf296dc4962b1
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-03 16:21:26 +0200

    Add files via upload

--- js/lesson23-events_forms_delegation/hw1-todo_List-createupdate_task/index.js
+++ js/lesson23-events_forms_delegation/hw1-todo_List-createupdate_task/index.js
@@ -1,9 +1,9 @@
 const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
+  { text: 'Buy milk', done: false, id: 1 },
+  { text: 'Pick up Tom from airport', done: false, id: 2 },
+  { text: 'Visit party', done: false, id: 3 },
+  { text: 'Visit doctor', done: true, id: 4 },
+  { text: 'Buy meat', done: true, id: 5 },
 ];
 
 const listElem = document.querySelector('.list');
@@ -28,22 +28,39 @@ const renderTasks = tasksList => {
 
   listElem.append(...tasksElems);
 };
-
+//---------додавання таски
 const taskInputField = document.querySelector('input');
 // const taskFromInput = taskInputField.value;
 
 const createTask = document.querySelector('.create-task-btn');
 
-const addNewTask = (event) => {
+const addNewTask = event => {
   const taskFromInput = event.target.value;
   console.log(taskFromInput);
-  tasks.push({ text: `${taskFromInput}`, done: false, id: Math.floor(Math.random() * 10) });
-    console.log(tasks);
-taskInputField.value.innerHTML = '';
+  tasks.push({ text: `${taskFromInput}`, done: false, id: Math.floor(Math.random(6) * 20) });
+  console.log(tasks);
+  taskInputField.value = '';
 };
 createTask.addEventListener('click', addNewTask);
 
+//---------------перевірка чекбокса
+
+const checkboxElem = document.querySelector('.list__item-checkbox');
+
+const changeDone = event => {
+  const isCheckbox = event.target.classList.contains('list__item-checkbox');
+  if (!isCheckbox) return;
+
+  const checkedEl = Number(event.target.dataset.id);
+  tasks.forEach(task => {
+    if (task.id === checkedEl) {
+      task.done = task.done === true ? false : true;
+    }
+    listElem.innerHTML = '';
+  });
+  renderTasks(tasks);
+};
+
+listElem.addEventListener('click', changeDone);
+
 renderTasks(tasks);
-
-
-
