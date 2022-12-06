b07b0d0a27124bf015e265e075a6760e5b849ad3
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-06 20:06:43 +0200

    Update index.js

--- js/lesson24-working_with_dates/hw1-date_difference/index.js
+++ js/lesson24-working_with_dates/hw1-date_difference/index.js
@@ -1,21 +1,18 @@
 
 export const getDiff = (startDate, endDate) => {
-let result;
-const diffDate = (new Date(startDate).getTime() - new Date(endDate).getTime());
-const daysData = Math.trunc(diffDate / 86400000);
-// console.log(daysData);
-const hoursData = Math.trunc((diffDate - daysData) / 3600000);
-// console.log(hoursData);
-const minutesData = Math.trunc((daysData - hoursData) / 60000);
-// console.log(minutesData);
-const secondsData = Math.trunc((hoursData - minutesData) / 1000);
-result = `${daysData}d ${hoursData}h ${minutesData}m ${secondsData}s`;
-// console.log(result);
-return result;
-}
-
-getDiff(new Date(2024, 11, 5, 23, 15, 7), new Date(2021, 0, 23, 10, 8, 7));
-
+  let result;
+  const diffDate = Math.abs(startDate).getTime() - new Date(endDate).getTime());
+  const daysData = Math.trunc(diffDate / 86400000);
+  const hoursData = new Date(diffDate).getUTCHours();
+  const minutesData = new Date(diffDate).getUTCMinutes();
+  const secondsData = new Date(diffDate).getUTCSeconds();
+  result = `${daysData}d ${hoursData}h ${minutesData}m ${secondsData}s`;
+  return result;
+};
+// getDiff(
+//   new Date(Date.UTC(2019, 10, 24, 20, 50, 34, 44)),
+//   new Date(Date.UTC(2019, 1, 22, 17, 52, 18)),
+// );
 
 
 
