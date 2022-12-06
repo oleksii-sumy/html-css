31d7eae1262d9a4dcc614fb0bce1b213d67a9f21
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-06 18:23:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-export const dayOfWeek = (date, days) => {
-  const inputDate = new Date(date).getDate();
-  const setDate = new Date(date).setDate(inputDate + days);
-  return week[new Date(setDate).getDay()];
-};
-const week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-// dayOfWeek(new Date(), 18);
+export const getDiff = (startDate, endDate) => {
+let result;
+const diffDate = (new Date(startDate).getTime() - new Date(endDate).getTime());
+const daysData = Math.trunc(diffDate / 86400000);
+// console.log(daysData);
+const hoursData = Math.trunc((diffDate - daysData) / 3600000);
+// console.log(hoursData);
+const minutesData = Math.trunc((daysData - hoursData) / 60000);
+// console.log(minutesData);
+const secondsData = Math.trunc((hoursData - minutesData) / 1000);
+result = `${daysData}d ${hoursData}h ${minutesData}m ${secondsData}s`;
+// console.log(result);
+return result;
+}
+
+getDiff(new Date(2024, 11, 5, 23, 15, 7), new Date(2021, 0, 23, 10, 8, 7));
+
+
+
+
