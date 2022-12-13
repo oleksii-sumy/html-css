64991b6caff22101440f109cc08fa10d66f679c3
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-13 10:24:17 +0200

    Update index.js

--- js/lesson28-functions_advanced_approaches/task4_from_lesson-composition/index.js
+++ js/lesson28-functions_advanced_approaches/task4_from_lesson-composition/index.js
@@ -2,9 +2,9 @@ const add2 = value => value + 2;
 const square = value => value * value;
 const half = value => value / 2;
 
-const compose = (...funcs) => value => {
+export const compose = (...funcs) => value => {
     return funcs.reduce((acc,func) => func(acc), value);
 };
 
-export const superFunc = compose(add2, square, half);
+ const superFunc = compose(add2, square, half);
 superFunc(2);
