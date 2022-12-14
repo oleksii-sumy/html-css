b3121f18fed555507945642da3173dce17b20ede
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-14 15:05:17 +0200

    Update index.js

--- js/lesson28-functions_advanced_approaches/hw1-function_shmoment/index.js
+++ js/lesson28-functions_advanced_approaches/hw1-function_shmoment/index.js
@@ -1,40 +1,26 @@
 export const shmoment = inputDate => {
   let resultDate = inputDate;
 
-  const getYears = inputDate.getFullYear();
-  const getMonths = inputDate.getMonth();
-  const getDays = inputDate.getDate();
-  const getHours = inputDate.getHours();
-  const getMinutes = inputDate.getMinutes();
-  const getSeconds = inputDate.getSeconds();
-  const getMilliseconds = inputDate.getMilliseconds();
 
   const getIntervalDate = {
-    years: getYears,
-    months: getMonths,
-    days: getDays,
-    hours: getHours,
-    minutes: getMinutes,
-    seconds: getSeconds,
-    milliseconds: getMilliseconds,
+    years: 'getYears',
+    months: 'getMonths',
+    days: 'getDays',
+    hours: 'getHours',
+    minutes: 'getMinutes',
+    seconds: 'getSeconds',
+    milliseconds: 'getMilliseconds',
   };
 
-  const setYears = inputDate.setFullYear();
-  const setMonths = inputDate.setMonth();
-  const setDays = inputDate.setDate();
-  const setHours = inputDate.setHours();
-  const setMinutes = inputDate.setMinutes();
-  const setSeconds = inputDate.setSeconds();
-  const setMilliseconds = inputDate.setMilliseconds();
 
   const setIntervalDate = {
-    years: setYears,
-    months: setMonths,
-    days: setDays,
-    hours: setHours,
-    minutes: setMinutes,
-    seconds: setSeconds,
-    milliseconds: setMilliseconds,
+    years: 'setYears',
+    months: 'setMonths',
+    days: 'setDays',
+    hours: 'setHours',
+    minutes: 'setMinutes',
+    seconds: 'setSeconds',
+    milliseconds: 'setMilliseconds',
   };
 
   const addSubtractMethods = {
@@ -44,7 +30,7 @@ export const shmoment = inputDate => {
       setIntervalDate.currentUnitValue(currentUnitValue + number);
       return addSubtractMethods;
     },
-    subtract(interval, number) {
+    Subtract(interval, number) {
       const currentUnitValue = resultDate[getIntervalDate[interval]]();
       setIntervalDate.currentUnitValue(currentUnitValue - number);
       return addSubtractMethods;
