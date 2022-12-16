20a382329a301922a465d9e0d24556c949af1f2d
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-16 22:27:42 +0200

    Add files via upload

---
+++ js/lesson29-asynchronous_code/task3-testing_event_loop/index.js
@@ -0,0 +1,19 @@
+export const printing = () => {
+  console.log(1);
+
+  setTimeout(function () {
+    console.log(5);
+  }, 1000);
+
+  console.log(2);
+
+  setTimeout(function () {
+    console.log(4);
+  }, 0);
+
+  setTimeout(function () {
+    console.log(6);
+  }, 2000);
+
+  console.log(3);
+};
