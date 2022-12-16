0a86b6562ae3d2d1835a9c430d6aa25312b78d2a
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-16 18:22:33 +0200

    Update index.js

--- js/lesson29-asynchronous_code/task1-load_image/index.js
+++ js/lesson29-asynchronous_code/task1-load_image/index.js
@@ -30,5 +30,4 @@ const onImageLoaded = (error, imgElem) => {
 
 addImage(
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+  onImageLoaded);
