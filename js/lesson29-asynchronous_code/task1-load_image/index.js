6671be3adb41f714b8a96bd138c7ac8ccf37c76f
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-16 18:00:51 +0200

    Update index.js

--- js/lesson29-asynchronous_code/task1-load_image/index.js
+++ js/lesson29-asynchronous_code/task1-load_image/index.js
@@ -1,4 +1,4 @@
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
   imgElem.setAttribute('alt', 'My photo');
   imgElem.src = imgSrc;
