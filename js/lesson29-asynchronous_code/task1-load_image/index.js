07a2cee2d899d51a66e3cd8aabe07e93fdd29c43
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-16 19:16:03 +0200

    Update index.js

--- js/lesson29-asynchronous_code/task1-load_image/index.js
+++ js/lesson29-asynchronous_code/task1-load_image/index.js
@@ -28,6 +28,6 @@ const onImageLoaded = (error, imgElem) => {
 };
 
 
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded);
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded);
