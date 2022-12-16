43ff45e12509068aaece31bb49e5a775f6cecc59
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-16 17:58:40 +0200

    Add files via upload

---
+++ js/lesson29-asynchronous_code/task1-load_image/index.js
@@ -0,0 +1,34 @@
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+const onImageLoaded = () => {
+    const {width, height} = imgElem;
+    callback(null, {width, height});
+};
+
+imgElem.addEventListener('load', onImageLoaded);
+imgElem.addEventListener('error',() => callback('Image load is failed'));
+};
+
+// callack function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
