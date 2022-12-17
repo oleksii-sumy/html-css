b2b27aed8deedbc8bf7da83da72388477daeb644
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-17 19:04:42 +0200

    Add files via upload

---
+++ js/lesson30-promise_intro/task1-from_lesson-promise-first_step/index.js
@@ -0,0 +1,27 @@
+export const addImage = (imgSrc) => {
+    const firstPromise = new Promise ((solution,errMessage) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    solution(imgElem);
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+  imgElem.addEventListener('error', () => errMessage ('Image load is failed'));
+});
+
+return firstPromise;
+};
+
+const resultAddImage = addImage(imgSrc);
+resultAddImage.then(data => console.log(data));
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
