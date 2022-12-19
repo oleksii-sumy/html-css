ad90e67ceaf4a73aeccb31054117f97dbc8450ce
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-19 17:43:31 +0200

    Add files via upload

---
+++ js/lesson31-promise_handlers_chaining/task1-from_lesson-promise_handlers/index.js
@@ -0,0 +1,20 @@
+export const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: 'userId777@example.com',
+        });
+      }, 1000);
+    }
+  });
+
+  return request;
+};
