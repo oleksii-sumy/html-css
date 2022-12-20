022f34f5d1151367cff0deb6fffb804b04c7b684
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-20 21:05:58 +0200

    Add files via upload

---
+++ js/lesson32-promise_static_methods/task2_from_lesson-promise_race/index.js
@@ -0,0 +1,23 @@
+const servers = ['https://server.com/us', 'https://server.com/eu', 'https://server.com/au'];
+
+const getRandomNUmber = (from, to) => from + Math.random() * (to - from);
+
+const request = url =>
+  new Promise(resolve => {
+    const randomDelay = getRandomNUmber(1000, 3000);
+    setTimeout(() =>
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+      }),
+    );
+  });
+
+export const getUserASAP = iserId => {
+  const userUrls = servers.map(serverUrl => `${serverUrl}/${serverUrl}`);
+
+  const requests = userUrls.map(userUrl => request(userUrl));
+  return Promise.race(requests);
+};
