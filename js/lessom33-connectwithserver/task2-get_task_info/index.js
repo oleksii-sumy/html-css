7b382f1185e7b36480f1ef8e395832993154b9bc
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-22 17:10:34 +0200

    Add files via upload

---
+++ js/lessom33-connectwithserver/task2-get_task_info/index.js
@@ -0,0 +1,19 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+function getTasksList() {
+  return fetch(`${baseUrl}`).then(response => response.json());
+}
+
+function getTaskById(taskId) {
+    return fetch(`${baseUrl}/${taskId}`).then(response => response.json());
+
+}
+
+// examples
+// getTasksList().then(tasksList => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
+
+// getTaskById('2').then(taskData => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
