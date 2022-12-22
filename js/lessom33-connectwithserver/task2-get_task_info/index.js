95bb3e09089bd4f143c5ef6d780d4d9dfc1f1059
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-22 17:12:03 +0200

    Update index.js

--- js/lessom33-connectwithserver/task2-get_task_info/index.js
+++ js/lessom33-connectwithserver/task2-get_task_info/index.js
@@ -1,13 +1,13 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-function getTasksList() {
+export function getTasksList() {
   return fetch(`${baseUrl}`).then(response => response.json());
 }
 
-function getTaskById(taskId) {
+export function getTaskById(taskId) {
     return fetch(`${baseUrl}/${taskId}`).then(response => response.json());
-
 }
+
 
 // examples
 // getTasksList().then(tasksList => {
