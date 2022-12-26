9c611dce6fff2ae52e445dd7ab5104e0291f8f7f
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-26 18:10:44 +0200

    Add files via upload

--- js/lesson34-complex_practice_with_http/hw1-_crud_requests_to_the_server/index.js
+++ js/lesson34-complex_practice_with_http/hw1-_crud_requests_to_the_server/index.js
@@ -9,7 +9,6 @@ export function getUserById(userId) {
 }
 
 export function createUser(userData) {
-  const { email, firstName, lastName, age } = userData;
   return fetch(baseUrl, {
     method: 'POST',
     headers: {
@@ -27,7 +26,7 @@ export function createUser(userData) {
 export function deleteUser(userId) {
   return fetch(`${baseUrl}/${userId}`, {
     method: 'DELETE',
-  }).then(response => response.json());
+  });
 }
 
 export function updateUser(userId, userData) {
@@ -37,6 +36,40 @@ export function updateUser(userId, userData) {
       'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(userData),
-  }).then(response => response.json());
+  });
 }
 
+// // examples
+// getUsersList().then(users => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then(userData => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('2').then(() => {
+//   console.log('User updated');
+// });
