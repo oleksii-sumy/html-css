9a4fcf90fd576b5b6c147b0285165e7c472a0e33
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-26 17:08:04 +0200

    Update index.js

--- js/lesson34-complex_practice_with_http/hw1-_crud_requests_to_the_server/index.js
+++ js/lesson34-complex_practice_with_http/hw1-_crud_requests_to_the_server/index.js
@@ -40,37 +40,3 @@ export function updateUser(userId, userData) {
   }).then(response => response.json());
 }
 
-// examples
-getUsersList().then(users => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
-
-getUserById('2').then(userData => {
-  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
-
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
-
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
-
-const updatedUserData = {
-  email: 'new@email.com',
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 17,
-};
-
-updateUser('1', updatedUserData).then(() => {
-  console.log('User updated');
-});
-
-deleteUser('2').then(() => {
-  console.log('User updated');
-});
