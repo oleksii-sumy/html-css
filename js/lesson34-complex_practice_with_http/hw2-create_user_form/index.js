9006a47623b9faaa814592aaa63f5f53b42b75cc
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2022-12-30 10:48:47 +0200

    Update index.js

--- js/lesson34-complex_practice_with_http/hw2-create_user_form/index.js
+++ js/lesson34-complex_practice_with_http/hw2-create_user_form/index.js
@@ -1,36 +1,48 @@
 const baseUrl = 'https://63a6c3fc59fd83b1bb3777cf.mockapi.io/todolist/userform';
 
 const checkValidation = () => {
-  const result = document.forms.reportValidity();
-  if (result == true) {
-    submitBtn.setAttribute('enabled', true);
-  }
-  getDataInput();
+  const result = new Promise(resolve => {
+    const resultChecked = loginForm.reportValidity();
+    if (resultChecked) {
+      submitBtn.removeAttribute('disabled');
+    }
+    resolve(resultChecked);
+  });
+  return result;
 };
 
-const getDataInput = () => {
-  const { fields } = document.forms;
-  const userData = Object.fromEntries(new FormData(fields));
-  sendToServer(userData);
-};
+const getDataInput = boolean => {
+  const resultData = new Promise(resolve => {
+    if (boolean) {
+      const { fields } = document.forms;
+      const userData = Object.fromEntries(new FormData(fields));
+      resolve(userData);
+    }
+  });
 
+  return resultData;
+};
 
 const sendToServer = data => {
-   fetch(baseUrl, {
+  fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(data),
   });
-    return alert(getFromServer);
+  return alert(getFromServer());
 };
 
-const submitBtn = document.querySelector('.submit-button');
-submitBtn.addEventListener('click', sendToServer);
+checkValidation()
+  .then(result => getDataInput(result))
+  .then(result => sendToServer(result));
 
 const getFromServer = () => fetch(baseUrl).then(response => response.json());
 
-const loginForm = document.querySelector('form');
+const submitBtn = document.querySelector('.submit-button');
+
+const loginForm = document.querySelector('.login-form');
 loginForm.addEventListener('input', checkValidation);
+
 
