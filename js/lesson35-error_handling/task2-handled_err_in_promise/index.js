68f6f97844d12b1bd6378d9c6025a6b171e15e7b
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-02 17:06:36 +0200

    Update index.js

--- js/lesson35-error_handling/task2-handled_err_in_promise/index.js
+++ js/lesson35-error_handling/task2-handled_err_in_promise/index.js
@@ -15,11 +15,11 @@ failRequest
   .catch(function onError2(error) {
     console.error('onError2', error.message);
     // (2)
-      
-throw err ('Sever error');
+return Promise.reject(new Error('Server error'));
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
+   
   })
   .catch(function onError3(error) {
     console.error('onError3', error.message);
