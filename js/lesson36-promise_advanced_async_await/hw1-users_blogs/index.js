fd75ca03524b3a77978efca94996a5dd01e6ac0e
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-04 16:05:52 +0200

    Update index.js

--- js/lesson36-promise_advanced_async_await/hw1-users_blogs/index.js
+++ js/lesson36-promise_advanced_async_await/hw1-users_blogs/index.js
@@ -1,16 +1,19 @@
 export const getUsersBlogs = async users => {
   let resLinks;
   try {
-  
-    const usersData = users
-    .map(user => {
-     const userData = fetch(`https://api.github.com/users/${user}`)
-       .catch(err => {
-         return Promise.reject(new Error(err.message));
-       })
-       .then(response => response.json())
-       .then(response => response.blog);
-        return userData;
+    const usersData = users.map(user => {
+      const userData = fetch(`https://api.github.com/users/${user}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to load data');
+          }
+          return response.json();
+        })
+        .then(response => response.blog)
+        .catch(err => {
+          return Promise.reject(new Error(err.message));
+        });
+      return userData;
     });
     resLinks = await Promise.all(usersData);
   } catch (err) {
@@ -19,6 +22,7 @@ export const getUsersBlogs = async users => {
     return resLinks;
   }
 };
+
 
 
 
