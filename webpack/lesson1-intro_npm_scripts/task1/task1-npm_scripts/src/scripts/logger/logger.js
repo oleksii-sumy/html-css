9b7940e973529582ade470b356887a52189141e2
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-09 17:37:08 +0200

    Add files via upload

---
+++ webpack/lesson1-intro_npm_scripts/task1/task1-npm_scripts/src/scripts/logger/logger.js
@@ -0,0 +1,16 @@
+export const createLogger = name => {
+  const logs = [];
+
+  return {
+    log(message) {
+      logs.push(`log - ${name} - ${message}`);
+    },
+    error(errorText) {
+              logs.push(`error - ${name} - ${errorText}`);
+
+    },
+    getLogs() {
+      return logs;
+    },
+  };
+};
