9c59bb2a4e6d8c7c77dc8f24c2fb7d814c2e8785
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-09 11:42:27 +0200

    Update logger.spec.js

--- webpack/lesson1-Intro.npm_scripts/task1-npm_scripts/src/scripts/logger/__tests__/logger.spec.js
+++ webpack/lesson1-Intro.npm_scripts/task1-npm_scripts/src/scripts/logger/__tests__/logger.spec.js
@@ -3,19 +3,19 @@ import {createLogger} from '../logger.js';
 it ('should return stored logs', () => {
     const logger = createLogger('user login');
 
-    expect(logger.getlogs()).toEqual([]);
+    expect(logger.getLogs()).toEqual([]);
 });
 
 it('should save log message', () => {
   const logger = createLogger('user login');
   logger.log('login success');
 
-  expect(logger.getlogs()).toEqual(['log - user - login success']);
+  expect(logger.getLogs()).toEqual(['log - user login - login success']);
 });
 
 it('should save errors', () => {
   const logger = createLogger('user login');
   logger.error('login failed');
 
-  expect(logger.getlogs()).toEqual(['error - user - login failed']);
+  expect(logger.getLogs()).toEqual(['error - user login - login failed']);
 });
