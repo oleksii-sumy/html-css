f00baa4cc1797ab90604f8a3b5ffc25dea913b94
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-11 11:14:53 +0200

    Delete profile.js

--- webpack/lesson2-babel_plugins/task1-babel_plugins/task1/dist/profile.js
+++
@@ -1,9 +0,0 @@
-cd; // file path: src/profile.js
-
-const printProfile = function (profileData) {
-  const {
-    name,
-    company
-  } = profileData;
-  console.log(`${name} from ${company}`);
-};
