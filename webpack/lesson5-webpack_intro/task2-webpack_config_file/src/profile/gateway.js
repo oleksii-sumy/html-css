d5fe5dfbb56158645510d84e25fe8175d1c2830a
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-13 14:34:23 +0200

    Add files via upload

---
+++ webpack/lesson5-webpack_intro/task2-webpack_config_file/src/profile/gateway.js
@@ -0,0 +1,8 @@
+export async function fetchUser(userId) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    return await response.json();
+  } catch (error) {
+    throw new Error('Failed to get user data');
+  }
+}
