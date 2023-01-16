2fb553da35d193a03fb9d945e3093d44920e3761
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-16 12:09:41 +0200

    Add files via upload

---
+++ webpack/lesson6-webpack_loaders/task1-webpack_css_loader/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name:'[path][name].[ext]',
+              outputPath:'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
