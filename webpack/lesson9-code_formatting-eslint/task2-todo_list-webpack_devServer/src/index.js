86e634efbdf37f9908b0771c4324f5e652dfb8bb
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-19 21:00:26 +0200

    Add files via upload

---
+++ webpack/lesson9-code_formatting-eslint/task2-todo_list-webpack_devServer/src/index.js
@@ -0,0 +1,24 @@
+import { initTodoListHandlers } from './list/todolist';
+import { renderTasks } from './list/render';
+import { getTasksList } from './list/tasksGateway';
+import { setItem } from './list/storage';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then((tasksList) => {
+    setItem('tasksList', tasksList);
+    renderTasks();
+  });
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
+
+// 1. get data from server
+// 2. save data to front-end storage
