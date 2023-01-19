03e1875ec9472ef248ea2caed99230331b55c47b
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-19 12:03:59 +0200

    Add files via upload

---
+++ webpack/lesson8-webpack_devServer/task2-todo_list-webpack_devServer/webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test:/.js$/,
+          use: ['babel-loader']
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                outputPath: 'images',
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+    ],
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
+
+  return config;
+};
