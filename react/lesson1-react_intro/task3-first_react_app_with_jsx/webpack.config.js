c36b6737836d56d4a3db43da63ba7efa3cafa554
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-23 22:03:56 +0200

    Add files via upload

---
+++ react/lesson1-react_intro/task3-first_react_app_with_jsx/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'review_build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
