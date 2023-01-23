a8540ec86f04ecc27253a28225c61ffa109ec8b3
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-23 22:13:55 +0200

    Add files via upload

--- react/lesson1-react_intro/task3-first_react_app_with_jsx/src/index.js
+++ react/lesson1-react_intro/task3-first_react_app_with_jsx/src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.querySelector('#root');
 const element = (
   <div className="greeting">
     <div className="greeting__title">Hello, world!</div>
-    <div className="greeting__text">I'm learning React!</div>
+    <div className="greeting__text">I'm learning React</div>
   </div>
 );
 
