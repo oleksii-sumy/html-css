cb8c00866ccfdf64214bd8b549611772c4bf01d7
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-24 12:20:57 +0200

    Add files via upload

---
+++ react/lesson1-react_intro/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(element, rootElement);
