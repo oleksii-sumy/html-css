ac8dc2834668aaa43d5491eb5484bc5ee4c1d64d
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-24 11:27:13 +0200

    Add files via upload

---
+++ react/lesson1-react_intro/task4-using_create_react_app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
+);
+
+ReactDOM.render(element, rootElement);
