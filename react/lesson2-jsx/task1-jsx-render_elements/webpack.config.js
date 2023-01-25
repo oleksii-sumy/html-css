279d6dab490ddfaf33d3655a830bd4f261e2f8c8
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-25 09:35:27 +0200

    Update webpack.config.js

--- react/lesson2-jsx/task1-jsx-render_elements/webpack.config.js
+++ react/lesson2-jsx/task1-jsx-render_elements/webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
-    entry: './src/index.jsx',
+    entry: './src/index.js',
     output: {
       filename: 'bundle.js',
     },
