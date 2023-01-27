795713c6281b3bcf09cfd1e5c4b00516d2e2d389
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-27 12:05:17 +0200

    Add files via upload

--- react/lesson3-components_props/task1-functional_components/src/Search.jsx
+++ react/lesson3-components_props/task1-functional_components/src/Search.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import './search.scss';
 
-const rootElement = document.querySelector('#root');
-
-const Search = (props) => {
+ const Search = (props) => {
   return (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
@@ -16,4 +10,5 @@ const Search = (props) => {
     </div>
   );
 };
-ReactDOM.render(<Search name="Tom" />, rootElement);
+
+export default Search;
