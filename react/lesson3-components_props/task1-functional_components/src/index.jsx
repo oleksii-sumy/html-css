c67fcee764ff0196c5cfc455bf5522abd0672241
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-27 10:51:13 +0200

    Add files via upload

---
+++ react/lesson3-components_props/task1-functional_components/src/index.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.scss';
+// import './search.scss';
+import './search.jsx';
+
+// const rootElement = document.querySelector('#root');
+// ReactDOM.render(<Search name="Tom" />, rootElement);
