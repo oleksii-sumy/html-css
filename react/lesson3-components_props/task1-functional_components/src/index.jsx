e8b557176078bf82041c29bdb1a51c13fd0fbd02
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-27 11:08:54 +0200

    Delete index.jsx

--- react/lesson3-components_props/task1-functional_components/src/index.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.scss';
-// import './search.scss';
-import './Search.jsx';
-
-// const rootElement = document.querySelector('#root');
-// ReactDOM.render(<Search name="Tom" />, rootElement);
