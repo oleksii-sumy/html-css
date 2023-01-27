a4fc3430773b8a06888e3b4c6e416abcd296ae9e
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-27 12:04:28 +0200

    Add files via upload

--- react/lesson3-components_props/task1-functional_components/webpack.config.js
+++ react/lesson3-components_props/task1-functional_components/webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
-    entry: './src/search.jsx',
+    entry: './src/index.jsx',
     output: {
       filename: 'bundle.js',
     },
