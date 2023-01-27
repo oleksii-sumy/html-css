ae0e896bb7c0cec92000dffc8c4a58c01f2fd9ff
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-27 13:03:03 +0200

    Add files via upload

---
+++ react/lesson3-components_props/task2-class_components/src/Search.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Search;
