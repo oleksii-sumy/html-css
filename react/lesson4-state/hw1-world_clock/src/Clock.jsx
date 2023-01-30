ef1b03c8b6e4784a00c3f419e2df72770944c159
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-01-30 16:56:09 +0200

    Add files via upload

--- react/lesson4-state/hw1-world_clock/src/Clock.jsx
+++ react/lesson4-state/hw1-world_clock/src/Clock.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './clock.scss';
+import moment from 'moment';
 
 const getTimeWithOffset = offset => {
   const currentTime = new Date();
   const utcOffset = currentTime.getTimezoneOffset() / 60;
   return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 };
+
+const formatDate = date => moment(date).format('h:mm:ss A');
 
 class Clock extends Component {
   constructor(props) {
@@ -16,7 +19,7 @@ class Clock extends Component {
     };
     setInterval(() => {
       this.setState({
-        offset: getTimeWithOffset(this.state.offset),
+        offset: getTimeWithOffset(this.props.offset),
       });
     }, 1000);
   }
@@ -25,7 +28,7 @@ class Clock extends Component {
     return (
       <div className="clock">
         <div className="clock__location">{this.state.location}</div>
-        <div className="clock__time">{this.state.offset}</div>
+        <div className="clock__time">{formatDate(this.state.offset)}</div>
       </div>
     );
   }
