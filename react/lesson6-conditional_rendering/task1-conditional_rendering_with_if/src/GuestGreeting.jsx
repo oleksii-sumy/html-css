bc4046b78055c04aa21283f389849fb2ac3f30a4
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-01 18:44:22 +0200

    Update GuestGreeting.jsx

--- react/lesson6-conditional_rendering/task1-conditional_rendering_with_if/src/GuestGreeting.jsx
+++ react/lesson6-conditional_rendering/task1-conditional_rendering_with_if/src/GuestGreeting.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GuestGreeting = props => {
     return (
       <h1>
-        Hi Stranger. Please login
+        Hi Stranger. Please login 🚀
       </h1>
     );
 };
