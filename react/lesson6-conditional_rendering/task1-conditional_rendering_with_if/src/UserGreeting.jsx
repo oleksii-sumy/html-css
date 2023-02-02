7ebeeb467271d11c12441b9d00382839f1ce710b
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-02 10:37:18 +0200

    Update UserGreeting.jsx

--- react/lesson6-conditional_rendering/task1-conditional_rendering_with_if/src/UserGreeting.jsx
+++ react/lesson6-conditional_rendering/task1-conditional_rendering_with_if/src/UserGreeting.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const UserGreeting = props => {
-  return (
-    <h1>
-      return <h1>Hello, user 😎</h1>;
-    </h1>
-  );
+  return <h1>Hello, user 😎</h1>;
 };
 
 export default UserGreeting;
