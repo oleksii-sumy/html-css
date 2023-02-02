ab5784bcda08d01c2946b6569e3c40c95f3f3b9b
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-02 17:47:09 +0200

    Add files via upload

--- react/lesson6-conditional_rendering/task3-info_about_products/src/Page.jsx
+++ react/lesson6-conditional_rendering/task3-info_about_products/src/Page.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Info from './Info';
 
-const text1 = 'IPhone 13 - Price is 500$. Available in 2 colors';
-const text2 = 'Price is 650$. Not available';
+const text1 = 'Price is 500$. Available in 2 colors';
+const text2 = 'Price is 650$. Not available in 1 color';
 
 class Page extends Component {
   constructor(props) {
@@ -22,8 +22,8 @@ return (
   <div className="page">
     <Info info={this.state.info}/>
     <div className="actions">
-      <button className="btn" onClick={()=> this.setInfo(text1)}> text1</button>
-      <button className="btn" onClick={()=> this.setInfo(text2)}> text2</button>
+      <button className="btn" onClick={()=> this.setInfo(text1)}> IPhone 13</button>
+      <button className="btn" onClick={()=> this.setInfo(text2)}> IPhone 13</button>
       <button className="btn" onClick={()=> this.setInfo('')}> Clear</button>
     </div>
   </div>
