1c8f342504077554ae05e88b312dcb5507b455c5
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-13 10:57:48 +0200

    Update UserForm.jsx

--- react/lesson9-forms/task2-controlled_component-users_form/src/UserForm.jsx
+++ react/lesson9-forms/task2-controlled_component-users_form/src/UserForm.jsx
@@ -19,7 +19,7 @@ class UserForm extends Component {
 
   handleSubmit = event =>{
     event.preventDefault();
-    console.log(this.state);
+    this.props.onSubmit(this.state);
   };
 
   render() {
