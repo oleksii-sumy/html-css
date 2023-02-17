4d95eb4ff1e77111dca9ed183662a982ca6adde1
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-17 23:40:42 +0200

    Update ProductList.jsx

--- react/lesson10-lifting_state_up/task2-products_list/src/ProductList.jsx
+++ react/lesson10-lifting_state_up/task2-products_list/src/ProductList.jsx
@@ -10,11 +10,11 @@ class ProductList extends Component {
           {this.props.cartItems.map(({ id, name, price }) => (
             <li key={id} className="products__list-item">
               <span className="products__item-name">{name}</span>
-              <span className="products__item-price">{price}</span>
+              <span className="products__item-price">${price}</span>
             </li>
           ))}
         </ul>
-        <div className="products__total">Total: {total}</div>
+        <div className="products__total">Total: ${total}</div>
       </div>
     );
   }
