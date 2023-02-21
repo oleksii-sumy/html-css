4f2cd9337c2b0c11a3382abfea322c2fa737b238
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-02-21 13:08:40 +0200

    Add files via upload

---
+++ react/lesson11-update_components/task1-update_component/src/Numbers.jsx
@@ -0,0 +1,21 @@
+import React, {PureComponent} from "react";
+
+class Numbers extends PureComponent {
+  // shouldComponentUpdate(nextProps) {
+  //   if (this.props.number === nextProps.number && this.props.title === nextProps.title) {
+  //   return false;
+  //   }
+  //       return true;
+
+  // }
+
+  render () {
+
+    return (
+      <div className="number">
+        <span className="number__title">{this.props.title}</span>
+        <span className="number__value">{this.props.number}</span>
+      </div>
+    );
+  }}
+  export default Numbers;
