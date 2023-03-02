c94809ea1f1a889ff116e084202a433abc6a083b
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-03-02 16:31:48 +0200

    Add files via upload

--- react/lesson13-react_router/task2-react_router_nested_routes/src/Product.jsx
+++ react/lesson13-react_router/task2-react_router_nested_routes/src/Product.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import {useParams} from 'react-router-dom';
 
-function Product () {
-  const {productId} = useParams();
-  return <div className="product">
-    {productId}
-    </div>;
+function Product({ match }) {
+  // const {productId} = useParams();
+  return <div className="product">{match.params.productId}</div>;
 }
 
 export default Product;
--- react/lesson13-react_router/task2-react_router_nested_routes/src/Products.jsx
+++ react/lesson13-react_router/task2-react_router_nested_routes/src/Products.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import Product from './Product';
 
-function Products({ match }) {
+function Products() {
   return (
     <div className="page__content">
       <h1>Products</h1>
       <ul className="navigation">
         <li className="navigation__item">
-          <Link to={`${match.url}/book`}>Book</Link>
+          <Link to="/products/book">Book</Link>
         </li>
         <li className="navigation__item">
-          <Link to={`${match.url}/ball`}>Ball</Link>
+          <Link to="/products/ball">Ball</Link>
         </li>
       </ul>
       <Switch>
-        <Route exact path={`${match.url}`}>
+        <Route exact path="/products">
           <span>Select a product please</span>
         </Route>
-        <Route path={`${match.url}/:productId`}>
-          <Product />
-        </Route>
+        <Route path="/products/:productId" component={Product} />
       </Switch>
     </div>
   );
