8e1ebeb77e86b57b0c7f8d752e192abb4d9af5d7
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-03-02 11:06:20 +0200

    Add files via upload

--- react/lesson13-react_router/task2-react_router_nested_routes/src/Products.jsx
+++ react/lesson13-react_router/task2-react_router_nested_routes/src/Products.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import {Route, Link, Switch} from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Product from './Product';
 
-function Products({match}) {
+function Products() {
   return (
     <div className="page__content">
       <h1>Products</h1>
       <ul className="navigation">
         <li className="navigation__item">
-          <Link to={`${match.url}/book`}>Book</Link>
+          <Link to="/products/book">Book</Link>
         </li>
         <li className="navigation__item">
-          <Link to={`${match.url}/ball`}>Ball</Link>
+          <Link to="/products/ball">Ball</Link>
         </li>
       </ul>
       <Switch>
-        <Route exact path={match.url}>
+        <Route exact path="/products">
           <span>Select a product please </span>
           <Product />
         </Route>
-        <Route>
-          <Product path={`${match.url}/:productId`} />
+        <Route path="/product/:productId">
           <Product />
         </Route>
       </Switch>
