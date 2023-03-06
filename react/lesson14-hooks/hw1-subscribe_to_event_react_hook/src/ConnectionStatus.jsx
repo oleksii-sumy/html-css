d134ebf19b4b367ffb4168a6441bf0cc22f45144
oleksii-sumy <111381003+oleksii-sumy@users.noreply.github.com>
2023-03-06 13:04:35 +0200

    Update ConnectionStatus.jsx

--- react/lesson14-hooks/hw1-subscribe_to_event_react_hook/src/ConnectionStatus.jsx
+++ react/lesson14-hooks/hw1-subscribe_to_event_react_hook/src/ConnectionStatus.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const ConnectionStatus = () => {
-  const [status, setStatus] = useState('onLine');
+  const [status, setStatus] = useState('online');
 
   useEffect(() => {
     const getOffStatus = e => {
